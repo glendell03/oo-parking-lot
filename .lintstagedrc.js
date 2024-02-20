e49a64a77c782ab267ce64050b5e19b7a1c5f21c
Glendell Bringino <dell@Glendells-MacBook-Pro.local>
2024-02-20 18:51:51 +0800

    chore: remove dlx

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,12 @@
-const path = require("path");
+const path = require('path')
 
-const biomeCheck = (filenames) =>
-  `pnpm dlx biome check --apply --organize-imports-enabled=true --verbose --colors=force ${filenames
-    .map((f) => `"${path.relative(process.cwd(), f)}"`)
-    .join(" ")}`;
+const biomeCheck = filenames =>
+	`pnpm biome check --apply --organize-imports-enabled=true --verbose --colors=force ${filenames
+		.map(f => `"${path.relative(process.cwd(), f)}"`)
+		.join(' ')}`
 
 module.exports = {
-  "*.{ts,tsx,js,jsx}": [biomeCheck],
+	'*.{ts,tsx,js,jsx}': [biomeCheck],
 
-  "**/*.{css,scss,md,html,json}": [biomeCheck],
-};
+	'**/*.{css,scss,md,html,json}': [biomeCheck]
+}
